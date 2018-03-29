f3918e2339331ce11b850f2b36905ee0ee55481b
github@cybertekschool.com <github@cybertekschool.com>
2018-03-28 20:11:13 -0400

    test data added

--- src/test/java/com/app/tests/BasicDataDrivenTest.java
+++ src/test/java/com/app/tests/BasicDataDrivenTest.java
@@ -1,15 +1,20 @@
 package com.app.tests;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.app.pages.GasMileageCalculatorPage;
+import com.app.utilities.ConfigurationReader;
 import com.app.utilities.Driver;
 
 public class BasicDataDrivenTest {
@@ -17,12 +22,24 @@ public class BasicDataDrivenTest {
 	WebDriver driver;
 	Workbook workbook;
 	Sheet worksheet;
+	FileInputStream inStream;
 	GasMileageCalculatorPage page;
+	
 	@BeforeClass
-	public void setUp() {
+	public void setUp() throws Exception{
+		inStream = 
+			new FileInputStream(ConfigurationReader.getProperty("gasmileage.testdata.path"));
+		workbook = WorkbookFactory.create(inStream);
+		worksheet = workbook.getSheetAt(0);
 		driver = Driver.getDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("http://www.calculator.net/gas-mileage-calculator.html");
+	}
+	
+	@AfterClass
+	public void tearDown() throws IOException {
+		inStream.close();
+		workbook.close();
 	}
 	
 	@Test
@@ -36,6 +53,7 @@ public class BasicDataDrivenTest {
 		page.previousOdometer.sendKeys(String.valueOf(previousOr));
 		page.gas.sendKeys(String.valueOf(gas));
 		page.calculate.click();
+		
 		String[] result = page.result.getText().split(" ");
 		System.out.println(result[0]);
 		

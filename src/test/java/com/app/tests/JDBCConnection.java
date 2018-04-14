899e5024ccf0b9f0bf2046aa87bd1d51a48e9220
github@cybertekschool.com <github@cybertekschool.com>
2018-04-14 15:15:42 -0400

    METADATA

--- src/test/java/com/app/tests/JDBCConnection.java
+++ src/test/java/com/app/tests/JDBCConnection.java
@@ -1,8 +1,10 @@
 package com.app.tests;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,32 +17,60 @@ public class JDBCConnection {
 	String oracleDbPassword = "hr";
 	
 	
-  @Test
+  @Test(enabled=false)
   public void oracleJDBC() throws SQLException {
 	  Connection connection=DriverManager.getConnection(oracleDbUrl, oracleDbUsername, oracleDbPassword); 
-	  Statement statement=connection.createStatement();
+	  //Statement statement=connection.createStatement();
+	  Statement statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 	  ResultSet resultSet = statement.executeQuery("select * from countries");
 
-	  resultSet.next();
+//	  while(resultSet.next()) {
+//		  System.out.println(resultSet.getString(1)+"-"+resultSet.getString("country_name")+"-"+resultSet.getInt("region_id"));	  	
+//	  }
+	  //find out how many records in the resultset
+	  resultSet.last();
+	  int rowsCount = resultSet.getRow();
+	  System.out.println("Number of rows:" + rowsCount);
 	  
-	  System.out.println(resultSet.getString(1));
-	  System.out.println(resultSet.getString("country_name"));
-	  System.out.println(resultSet.getInt("region_id"));	  
-		
-	  resultSet.next();
-	  System.out.println(resultSet.getString(1));
-	  System.out.println(resultSet.getString("country_name"));
-	  System.out.println(resultSet.getInt("region_id"));	 
-	  
-	  resultSet.next();
-	  System.out.println(resultSet.getString(1));
-	  System.out.println(resultSet.getString("country_name"));
-	  System.out.println(resultSet.getInt("region_id"));	 
+	  resultSet.first();
+	  while(resultSet.next()) {
+		  System.out.println(resultSet.getString(1)+"-"+resultSet.getString("country_name")+"-"+resultSet.getInt("region_id"));	  	
+	  }
 	  
 	  resultSet.close();	  
 	  statement.close();	  
 	  connection.close();
   }
+  
+  @Test
+  public void jdbcMetadata() throws Exception{
+	  Connection connection=DriverManager.getConnection(oracleDbUrl, oracleDbUsername, oracleDbPassword); 
+	  //Statement statement=connection.createStatement();
+	  Statement statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+	  
+	  String sql = "select employee_id,last_name,job_id, salary from employees";
+	  //String sql = "select * from employees";
+	  ResultSet resultSet = statement.executeQuery(sql);
+	  
+	  //Database metadata
+	  DatabaseMetaData dbMetadata = connection.getMetaData();
+	  System.out.println("User:" + dbMetadata.getUserName());
+	  System.out.println("Database type:" + dbMetadata.getDatabaseProductName());
+	  //resultSet metadata
+	  ResultSetMetaData rsMedata = resultSet.getMetaData();
+	  System.out.println("Columns count:" + rsMedata.getColumnCount());
+	  System.out.println( rsMedata.getColumnName(1));
+	  //print all column names using a loop
+	  for (int i = 1; i <= rsMedata.getColumnCount() ; i++) {
+		System.out.println(i +" -> " + rsMedata.getColumnName(i));
+	  }
+	  
+	  resultSet.close();	  
+	  statement.close();	  
+	  connection.close();
+  }
+  
+  
 }
 
 

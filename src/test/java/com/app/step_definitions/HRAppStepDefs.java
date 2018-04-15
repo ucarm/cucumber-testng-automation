c1ecf7875e021940a3473ad568573418b035bf82
github@cybertekschool.com <github@cybertekschool.com>
2018-04-15 14:41:24 -0400

    assert ui vs db

--- src/test/java/com/app/step_definitions/HRAppStepDefs.java
+++ src/test/java/com/app/step_definitions/HRAppStepDefs.java
@@ -1,10 +1,19 @@
 package com.app.step_definitions;
 
+import static org.testng.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 
 import com.app.pages.HRAppDeptEmpPage;
 import com.app.utilities.BrowserUtils;
 import com.app.utilities.ConfigurationReader;
+import com.app.utilities.DBType;
+import com.app.utilities.DBUtility;
 import com.app.utilities.Driver;
 
 import cucumber.api.PendingException;
@@ -17,6 +26,9 @@ public class HRAppStepDefs {
 	private WebDriver driver = Driver.getDriver();
 	private HRAppDeptEmpPage deptEmpPage=new HRAppDeptEmpPage();
 	
+	private Map<String,String> UIDepartmentData = new HashMap<>();
+	private Map<String,Object> DBDepartmentData = new HashMap<>();
+	
 	@Given("^I am on DeptEmpPage$")
 	public void i_am_on_DeptEmpPage() {
 		driver.get(ConfigurationReader.getProperty("hrapp.url"));
@@ -27,27 +39,31 @@ public class HRAppStepDefs {
 	    int currentDepId = Integer.parseInt(deptEmpPage.departmentID.getText());
 	    
 	    while(currentDepId != deptID) {
-	    		
 	    		deptEmpPage.Next.click();
 	    		BrowserUtils.waitFor(2);
 	    		deptEmpPage = new HRAppDeptEmpPage();
 	    		BrowserUtils.waitForVisibility(deptEmpPage.departmentID, 5);
-	    		currentDepId = Integer.parseInt(deptEmpPage.departmentID.getText());
-	    		
+	    		currentDepId = Integer.parseInt(deptEmpPage.departmentID.getText());  		
 	    }
-	    
+	    //add UI data to hashmap
+	    UIDepartmentData.put("DEPARTMENT_NAME", deptEmpPage.departmentName.getText());
+	    UIDepartmentData.put("MANAGER_ID", deptEmpPage.managerID.getText());
+	    UIDepartmentData.put("LOCATION_ID", deptEmpPage.locationID.getText());  
 	}
 
 	@When("^I query database with sql \"([^\"]*)\"$")
-	public void i_query_database_with_sql(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_query_database_with_sql(String sql) throws SQLException {
+	    DBUtility.establishConnection(DBType.ORACLE);
+	    List<Map<String,Object>> DBDataList = DBUtility.runSQLQuery(sql);
+	    DBDepartmentData = DBDataList.get(0);
+	    DBUtility.closeConnections();
 	}
 
 	@Then("^UI data and Database data must match$")
 	public void ui_data_and_Database_data_must_match() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		assertEquals(UIDepartmentData.get("DEPARTMENT_NAME"),DBDepartmentData.get("DEPARTMENT_NAME"));
+		assertEquals(UIDepartmentData.get("LOCATION_ID"),String.valueOf(DBDepartmentData.get("LOCATION_ID")));
+		assertEquals(UIDepartmentData.get("MANAGER_ID"),String.valueOf(DBDepartmentData.get("MANAGER_ID")));
 	}
 
 }

696a76366fa8faa316ccbf7f49a7b6a5a419af61
github@cybertekschool.com <github@cybertekschool.com>
2018-04-21 16:01:44 -0400

    jsonpath

--- src/test/java/com/app/tests/APiDay3_JsonPath.java
+++ src/test/java/com/app/tests/APiDay3_JsonPath.java
@@ -2,12 +2,19 @@ package com.app.tests;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.testng.annotations.Test;
 
 import com.app.utilities.ConfigurationReader;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 public class APiDay3_JsonPath {
 
@@ -82,20 +89,92 @@ public class APiDay3_JsonPath {
 								"last_name",equalTo("Chen"),
 								"email",equalTo("JCHEN")
 								);
+
+	}
+	
+	
+	/*
+	 * Given Accept type is Json
+	 * And Params are limit=100
+	 * When I send get request to 
+	 * http://34.223.219.142:1212/ords/hr/employee 
+	 * Then status code is 200 
+	 * And Response content should be json 
+	 * all employee ids should be returned
+	 */
+	
+	@Test
+	public void testWithJsonPath() {
+		
+		Map<String,Integer> rParamMap = new HashMap<>();
+		rParamMap.put("limit", 100);
+		
+		Response response = given().accept(ContentType.JSON)//header
+							.and().params(rParamMap) //query param/request param
+							.and().pathParams("employee_id", 177) //path param
+							.when().get(ConfigurationReader.getProperty("hrapp.baseresturl")+"/employees/{employee_id}");
+		
+		JsonPath json = response.jsonPath(); //get json body and assign to jsonPath object
+		
+		System.out.println(json.getInt("employee_id"));
+		System.out.println(json.getString("last_name"));
+		System.out.println(json.getString("job_id"));
+		System.out.println(json.getInt("salary"));
+		System.out.println(json.getString("links[1].href")); //get specific element from array
+		//assign all hrefs into a list of strings
+		List<String> hrefs = json.getList("links.href");
+		
+		System.out.println(hrefs);
+	}	
+	
+	/*
+	 * Given Accept type is Json
+	 * And Params are limit=100
+	 * When I send get request to 
+	 * http://34.223.219.142:1212/ords/hr/employee 
+	 * Then status code is 200 
+	 * And Response content should be json 
+	 * all employee data should be returned
+	 */
+	
+	@Test
+	public void testJsonPathWithLists() {
+		Map<String,Integer> rParamMap = new HashMap<>();
+		rParamMap.put("limit", 100);
+		
+		Response response = given().accept(ContentType.JSON)//header
+							.and().params(rParamMap) //query param/request param
+							.when().get(ConfigurationReader.getProperty("hrapp.baseresturl")+"/employees");
+		
+		assertEquals(response.statusCode(),200); //check status code
+		
+		JsonPath json = response.jsonPath();
+		//JsonPath json = new JsonPath(new File(FilePath.json));
+		//JsonPath json = new JsonPath(response.asString());
+		
+		//get all employee ids into an arraylist
+		List<Integer> empIds = json.getList("items.employee_id");
+		System.out.println(empIds);
+		//assert that there are 100 emp ids
+		
+		assertEquals(empIds.size(),100);
+		
+		//get all emails and assign into arraylist
+		List<String> emails = json.getList("items.email");
+		System.out.println(emails);
+		
+		assertEquals(emails.size(),100);
+		
+		//get all employee ids that are greater than 150
+		List<String> empIdList = json.getList("items.findAll{it.employee_id > 150}.employee_id");
+		System.out.println(empIdList);
+		
+		//get all employee lastnames, whose salary is more than 7000
+		
 		
 		
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	

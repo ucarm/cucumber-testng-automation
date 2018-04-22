7f31223087825e0783718410fe0273551db2c00b
github@cybertekschool.com <github@cybertekschool.com>
2018-04-22 15:20:25 -0400

    response pojo

--- src/test/java/com/app/beans/RegionReponse.java
+++ src/test/java/com/app/beans/RegionReponse.java
@@ -1,13 +1,24 @@
 package com.app.beans;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class RegionReponse {
 	private int region_id;
 	private String region_name;
+	private ArrayList<Map<String,String>> links;
 	
 	
+	public ArrayList<Map<String, String>> getLinks() {
+		return links;
+	}
+
+	public void setLinks(ArrayList<Map<String, String>> links) {
+		this.links = links;
+	}
 
 	public void setRegion_id(int region_id) {
 		this.region_id = region_id;

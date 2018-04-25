8a2004107fea1281388f089cdfae1ff1f80f1c0d
github@cybertekschool.com <github@cybertekschool.com>
2018-04-24 22:10:04 -0400

    cucumber+restAssured

--- src/test/java/com/app/step_definitions/api/API_PostAnEmployee.java
+++ src/test/java/com/app/step_definitions/api/API_PostAnEmployee.java
@@ -5,40 +5,91 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import com.app.utilities.ConfigurationReader;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+
 public class API_PostAnEmployee {
+	
+	RequestSpecification request;
+	int employeeId;
+	Response response;
+	String url = ConfigurationReader.getProperty("hrapp.baseresturl") + "/employees/";
+	Map reqEmployee;
+	
 	@Given("^Content type and Accept type is Json$")
 	public void content_type_and_Accept_type_is_Json() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		request = given().accept(ContentType.JSON)
+				.and().contentType(ContentType.JSON);
 	}
 
 	@When("^I post a new Employee with \"([^\"]*)\" id$")
-	public void i_post_a_new_Employee_with_id(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_post_a_new_Employee_with_id(String id) {
+	    if(id.equals("random")) {
+	    		employeeId = new Random().nextInt(999999);
+	    }else {
+	    		employeeId = Integer.parseInt(id);
+	    }
+	    
+	    reqEmployee = new HashMap();
+		reqEmployee.put("employee_id", employeeId);
+		reqEmployee.put("first_name","POSTNAME");
+		reqEmployee.put("last_name","POSTLNAME");
+		reqEmployee.put("email","EM"+employeeId);
+		reqEmployee.put("phone_number", "202.123.4567");
+		reqEmployee.put("hire_date","2018-04-24T07:25:00Z");
+		reqEmployee.put("job_id","IT_PROG");
+		reqEmployee.put("salary",7000);
+		reqEmployee.put("commission_pct",null);
+		reqEmployee.put("manager_id",null);
+		reqEmployee.put("department_id",90);
+		
+		response =request.body(reqEmployee)
+				.when().post(url);
 	}
 
 	@Then("^Status code is (\\d+)$")
-	public void status_code_is(int arg1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void status_code_is(int statusCode) {
+	    assertEquals(response.statusCode(),statusCode);
 	}
 
 	@Then("^Response Json should contain Employee info$")
 	public void response_Json_should_contain_Employee_info() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		Map postResEmployee = response.body().as(Map.class);
+		//	And Response Json should contain Employee info
+		for(Object key : reqEmployee.keySet()) {
+			System.out.println(postResEmployee.get(key)+" <> "+reqEmployee.get(key));
+			assertEquals(postResEmployee.get(key),reqEmployee.get(key));
+		}
 	}
 
-	@When("^i send a GET request with \"([^\"]*)\" id$")
-	public void i_send_a_GET_request_with_id(String arg1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@When("^I send a GET request with \"([^\"]*)\" id$")
+	public void i_send_a_GET_request_with_id(String id) {
+		if(!id.equals("random")) {
+			employeeId = Integer.parseInt(id);
+		}
+		response = given().accept(ContentType.JSON)
+				   .when().get(url+employeeId);
 	}
 
-	@Then("^employee JSON Response Data should match the posted JSON data$")
+	@Then("^Employee JSON Response Data should match the posted JSON data$")
 	public void employee_JSON_Response_Data_should_match_the_posted_JSON_data() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		Map getResMap = response.body().as(Map.class);
+		
+		for(Object key:reqEmployee.keySet()) {
+			System.out.println(key + ": " +reqEmployee.get(key) + "<>" +getResMap.get(key));
+			assertEquals(getResMap.get(key),reqEmployee.get(key));
+		}
 	}
 }

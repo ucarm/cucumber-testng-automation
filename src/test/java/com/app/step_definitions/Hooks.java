281266d4fb2b501e1e170c25bc00e84dffb7992c
github@cybertekschool.com <github@cybertekschool.com>
2018-04-29 17:27:13 -0400

    parallel execution

--- src/test/java/com/app/step_definitions/Hooks.java
+++ src/test/java/com/app/step_definitions/Hooks.java
@@ -18,7 +18,7 @@ public class Hooks {
 	public void setUp(Scenario scenario) {
 		WebDriver driver = Driver.getDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.manage().window().fullscreen();
+		//driver.manage().window().fullscreen();
 	}
 
 	@After
--- src/test/java/com/app/utilities/Driver.java
+++ src/test/java/com/app/utilities/Driver.java
@@ -1,8 +1,14 @@
 package com.app.utilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,11 +21,20 @@ public class Driver {
 	public static WebDriver getDriver() {
 		if (driver == null) {
 			switch (ConfigurationReader.getProperty("browser")) {
-			case "firefox":
-				WebDriverManager.firefoxdriver().setup();
-				driver = new FirefoxDriver();
-				break;
 			case "chrome":
+//				WebDriverManager.firefoxdriver().setup();
+//				
+//				driver = new FirefoxDriver();
+				DesiredCapabilities caps = DesiredCapabilities.chrome();
+				caps.setPlatform(Platform.ANY);
+				try {
+					driver = new RemoteWebDriver(new URL("http://52.70.143.199:4444/wd/hub"),caps);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case "firefox":
 				WebDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
 				break;
